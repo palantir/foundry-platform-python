a97d3ba7115c491d4428d1afc2008c881498f503
Jacob <jacobas@palantir.com>
2024-05-29 18:08:20 -0300

    Add set_version.py script (#7)

---
+++ scripts/set_version.py
@@ -0,0 +1,17 @@
+import subprocess
+
+gitversion = subprocess.check_output("git describe --tags --abbrev=0".split()).decode().strip()
+
+print(f"Setting version to {gitversion}...")
+
+path = "foundry/_versions.py"
+
+with open(path, "r") as f:
+    content = f.read()
+
+content = content.replace('__version__ = "0.0.0"', f'__version__ = "{gitversion}"')
+
+with open(path, "w") as f:
+    f.write(content)
+
+print("Done!")
