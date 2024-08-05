361f7fdac3235b014b3646ab4fda95b6ac86eda2
Jacob Smith <jacobas@palantir.com>
2024-08-05 08:52:38 -0300

    Add test

--- test/models/test_transaction.py
+++ test/models/test_transaction.py
@@ -18,7 +18,7 @@ from datetime import timezone
 from foundry.models import Transaction
 
 
-def test_model_validate():
+def test_model_validate_with_z():
     r = Transaction.model_validate(
         {
             "rid": "ri.foundry.main.transaction.00000024-065b-a1ba-9d5c-805d236318ad",
@@ -30,3 +30,17 @@ def test_model_validate():
     assert r.transaction_type == "SNAPSHOT"
     assert r.status == "OPEN"
     assert r.created_time == datetime(2024, 8, 1, 17, 28, 58, 796000, tzinfo=timezone.utc)
+
+
+def test_model_validate_with_two_digit_milli():
+    r = Transaction.model_validate(
+        {
+            "rid": "ri.foundry.main.transaction.00000024-065b-a1ba-9d5c-805d236318ad",
+            "transactionType": "SNAPSHOT",
+            "status": "OPEN",
+            "createdTime": "2024-08-02T20:19:30.93+00:00",
+        }
+    )
+    assert r.transaction_type == "SNAPSHOT"
+    assert r.status == "OPEN"
+    assert r.created_time == datetime(2024, 8, 2, 20, 19, 30, 930000, tzinfo=timezone.utc)
